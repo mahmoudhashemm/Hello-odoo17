26873aa8a371f74489e5f01567e297102f597285
mahmoudhashem <mahmoud.metto@gmail.com>
2024-10-24 22:02:47 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
-port8096=$4
+port8069=$4
 port8072=$5
 # clone Odoo directory
 git clone --depth=1 https://github.com/mahmoudhashemm/Hello-odoo17.git $DESTINATION
@@ -17,7 +17,7 @@ if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F 
 sudo sysctl -p
 sed -i 's/10019/'$PORT'/g' $DESTINATION/docker-compose.yml
 sed -i 's/20014/'$CHAT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/8069/'$PORT8096'/g' $DESTINATION/docker-compose.yml
+sed -i 's/8069/'$PORT8069'/g' $DESTINATION/docker-compose.yml
 sed -i 's/8072/'$port8072'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
